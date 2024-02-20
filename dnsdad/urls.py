002750057e97c6aeaf524bda6ecd0a4ff5cfa2f2
sagartankw3nuts <sagartank.w3nuts@gmail.com>
2024-02-20 12:01:47 +0530

    add image route

--- dnsdad/urls.py
+++ dnsdad/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
 ]
 
 # Serve media files during development
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
